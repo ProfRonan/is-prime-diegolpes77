f49451fe91ceaed56503d401138bc9ffecfd4546
Diego Lopes <dieolpes77@gmail.com>
2023-05-31 10:26:37 -0300

    feito - correção

---
+++ main.py
@@ -0,0 +1,17 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+num = int(input("Digite um número inteiro: "))
+
+if num <= 0:
+    print("Número inválido")
+else:
+    if is_prime(num):
+        print("Primo")
+    else:
+        print("Não primo")
